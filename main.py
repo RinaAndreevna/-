3aefeed9c5bbad9ec9d379377d70aec8663daab0
RinaAndreevna <117077257+RinaAndreevna@users.noreply.github.com>
2022-11-03 20:19:01 +0300

    Add files via upload
    text

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-import json
+import os
+from typing import List
 
-keys = ['ingridient_name', 'quantity', 'measure', ]
-cook_book_dict = {}
+file_list = ['1.txt', '2.txt', '3.txt']
+new_file_list = ['n1.txt', 'n2.txt', 'n3.txt']
+new_data_from_files = []
+step = 0
+while step < len(file_list):
+    for some_file in file_list:
+        with open(file_list[step], encoding='utf -8') as f:
+            data = f.readlines()
+            qvon_str = len(data)
+        with open(file_list[step], encoding='utf -8') as f:
+            data_text = f.read()
 
-with open('Recipes.txt') as text:
-    lines = []
-    for line in text:
-        line = line.strip()
-        if line:
-            lines.append(line)
-        continue
-    lines = iter(lines) 
-    for name in lines:  
-        cook_book_dict[name] = []
-        num = next(lines)  
+    for some_file in new_file_list:
+        with open(new_file_list[step], 'w', encoding='utf -8') as f:
+            f.write(f'{file_list[step]}\n')
+            f.write(f'{qvon_str}\n')
+            f.write(f'{data_text}\n')
+    step += 1
+for some_file in new_file_list:
+    with open(some_file) as f:
+        new_data = f.read()
+        new_data_from_files.append(new_data)
+new_data_from_files.sort(key=len)
+sorted(new_data_from_files, key=len)
+print(new_data_from_files)
 
-        for _ in range(int(num)):  
-            sostav_line = next(lines)  
-            ingrid = sostav_line.split(' | ')  
-            z = zip(keys, ingrid)  
-            sostav_dict = {k: v for (k, v) in z} 
-            cook_book_dict[name].append(sostav_dict)
-            continue
-
-       
-        continue
-
-print(json.dumps(cook_book_dict, indent=2, ensure_ascii=False))
+with open('finish_file.txt', 'a', encoding='utf -8') as f:
+    for some_pat in new_data_from_files:
+        f.write(f'{some_pat}')
